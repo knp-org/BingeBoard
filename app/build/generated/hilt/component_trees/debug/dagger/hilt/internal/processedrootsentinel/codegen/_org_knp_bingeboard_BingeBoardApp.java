a8d7d95e5e92fe11a8f13fe45b4309461d680fc7
knp-org <knp-org@users.noreply.github.com>
2026-03-06 16:10:39 +0530

    Cleanup: Remove accidentally committed build and gradle files from tracking

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_org_knp_bingeboard_BingeBoardApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "org.knp.bingeboard.BingeBoardApp"
-)
-public final class _org_knp_bingeboard_BingeBoardApp {
-}
