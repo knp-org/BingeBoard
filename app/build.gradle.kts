59723f4e81404082cab5e642d940a434d798d539
knp-org <knp-org@users.noreply.github.com>
2026-05-24 12:19:08 +0530

    chore: bump app version to 1.3.0 and increment version code to 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "org.knp.bingeboard"
         minSdk = 33
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
